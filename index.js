29619fbe0e3c2988571dc9da9f4538c92027e9b8
Angel H <angel@adboomgrp.com>
2018-09-09 22:48:57 -0700

    pushed index.js file up for homework assignment #1

---
+++ index.js
@@ -0,0 +1,31 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    // data emitt event
+    req.on('data', data => {});
+
+    // request end event
+    req.on('end', () => {
+        router.hello((statusCode, payload) => {
+            //  Covert the payload to a string
+            var payloadString = JSON.stringify(payload);
+
+            // Return the response
+            res.setHeader('Content-Type', 'application/json');
+            res.writeHead(statusCode);
+            res.end(payloadString);
+        });
+    });
+})
+
+// Router that assigns pathname handlers
+var router = {
+    hello: (callback) => {
+        callback(200, {'message' : 'Hello there, Pirple!'});
+    }
+};
+
+// Start the server
+server.listen(3000, () => {
+    console.log('The server is listening on port 3000 now')
+});
